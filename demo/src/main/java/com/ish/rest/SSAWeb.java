741c586503f78db695e6da3cd4b9a3d7a3ac1bf1
unknown <madyasha460@gmail.com>
2025-04-17 20:50:31 +0530

    Initial commit

---
+++ demo/src/main/java/com/ish/rest/SSAWeb.java
@@ -0,0 +1,37 @@
+package com.ish.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ssa-web-api")
+public class SSAWeb {
+
+    @GetMapping("/find/{ssn}")
+    public ResponseEntity<String> getStateBySSN(@PathVariable Integer ssn){
+        if (String.valueOf(ssn).length()!=9)
+            return new ResponseEntity<String>("Invalid ssn", HttpStatus.BAD_REQUEST);
+
+        //get state name
+        int stateCode = ssn%100;
+        String stateName = null;
+
+        if (stateCode == 1)
+            stateName = "Washington DC";
+        else if (stateCode == 2)
+            stateName = "Ohio";
+        else if (stateCode == 3)
+            stateName = "Texas";
+        else if (stateCode == 4)
+            stateName = "California";
+        else if (stateCode == 5)
+            stateName = "Florida";
+        else
+            stateName="Invalid ssn";
+      return new ResponseEntity<String>(stateName,HttpStatus.OK);
+    }
+}
